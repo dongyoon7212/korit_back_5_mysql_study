d6ddc61f6936d70163ccc4d0299a23e2f71a442a
dongyoon7212 <dongyoon7212@naver.com>
2024-01-22 16:47:03 +0900

    trigger, procedure 추가 설명

--- procedure.sql
+++ procedure.sql
@@ -7,3 +7,10 @@ call p_sum(20, 30, @result, @count);
 
 select
 	@count;
+    
+# Procedures -> 매개변수로 뭘 주느냐에 따라 달라짐
+# = ` 안에는 p_ 로 시작`
+# in = 입력, out = 출력, inout = input,output으로 사용
+# PROCEDURE에는 return X , out이 출력 여러개도 가능
+# BEGIN
+# declare(지역변수) = _(언더바)로 시작
--- user_tb_trigger.sql
+++ user_tb_trigger.sql
@@ -16,3 +16,14 @@ from
 	user_mst_tb
 where
 	username = 'abc';
+    
+# 트리거 설정 = table 설정 - > Triggers
+# master table 기준
+# BEFORE INSERT = insert되기전 항상 실행
+# 이벤트
+# AFTER INSERT = insert가 되고난 후 실행 new. / mst 프라이머리키가 생성이 된후(키값 생성후) ->dtl에서 key값을 받아야하기때문에 AFTER INSERT
+# BEFORE DELETE = old.
+# INSERT = AFTER
+# DELETE = BEFORE
+# UPDATE =  AFTER BEFORE 두가지 다 사용
+# delete를 하더라도 데이터를 다 지우는 것이 아닌 old. 으로 다른곳으로 옮기는것

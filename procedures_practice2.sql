c4d399d1d612ad76074d7ce958c51f4871cc6d60
dongyoon7212 <dongyoon7212@naver.com>
2024-01-23 11:20:12 +0900

    procedures, transaction

---
+++ procedures_practice2.sql
@@ -0,0 +1,13 @@
+set @book_name = '송사무장의 실전경매';
+set @author_name = '송희창';
+set @publisher_name = '지혜로';
+set @new_book_id = 0;
+
+call p_insert_book(@book_name, @author_name, @publisher_name, @new_book_id);
+
+select
+	*
+from
+	book_tb
+order by
+	book_id desc;

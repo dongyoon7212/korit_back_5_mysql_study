734ef20b5bb9e8d7dfd9fe86a79cca206735eb85
dongyoon7212 <dongyoon7212@naver.com>
2024-01-23 12:05:12 +0900

    procedures transaction 수정

--- procedures_practice2.sql
+++ procedures_practice2.sql
@@ -1,9 +1,9 @@
-set @book_name = '송사무장의 실전경매';
-set @author_name = '송희창';
-set @publisher_name = '지혜로';
-set @new_book_id = 0;
+set @book_name = '송사무장의 실전경매2';
+set @author_name = '송희창2';
+set @publisher_name = '지혜로2';
+set @insert_book_status = 0;
 
-call p_insert_book(@book_name, @author_name, @publisher_name, @new_book_id);
+call p_insert_book(@book_name, @author_name, @publisher_name, @insert_book_status);
 
 select
 	*
@@ -11,3 +11,5 @@ from
 	book_tb
 order by
 	book_id desc;
+    
+select @insert_book_status;
